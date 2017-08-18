1d423700d37706f32ca133a82705a7f39b132641
Nicholas Clark <nbclark@gmail.com>
2017-08-18 13:17:58 -0700

    Small fixes

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,6 @@ var _client = require('./client');
 
 var _client2 = _interopRequireDefault(_client);
 
-var _view = require('./view');
-
-var _view2 = _interopRequireDefault(_view);
-
 var _ddWeb = require('./dd-web');
 
 var _ddWeb2 = _interopRequireDefault(_ddWeb);
@@ -18,4 +14,4 @@ var _connectivityBanner2 = _interopRequireDefault(_connectivityBanner);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-module.exports = { Client: _client2.default, View: _view2.default, WebShim: _ddWeb2.default, ConnectivityBanner: _connectivityBanner2.default };
+module.exports = { Client: _client2.default, WebShim: _ddWeb2.default, ConnectivityBanner: _connectivityBanner2.default };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Client from './client'
-import View from './view'
 import WebShim from './dd-web'
 import ConnectivityBanner from './connectivity-banner'
 
-module.exports = { Client, View, WebShim, ConnectivityBanner }
+module.exports = { Client, WebShim, ConnectivityBanner }
